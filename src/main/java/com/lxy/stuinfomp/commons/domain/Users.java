ed4aa8392d53aa0f5ac2d839cabf3522f8969944
robin_time <robin_time@163.com>
2019-07-28 17:44:16 +0800

    用户注册接口

--- src/main/java/com/lxy/stuinfomp/commons/domain/Users.java
+++ src/main/java/com/lxy/stuinfomp/commons/domain/Users.java
@@ -1,30 +1,27 @@
 package com.lxy.stuinfomp.commons.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.lxy.stuinfomp.commons.dto.AbstractBaseDomain;
 import com.lxy.stuinfomp.commons.utils.RegexpUtils;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.Date;
 
 @Table(name = "users")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Users extends AbstractBaseDomain {
-    /**
-     * 主键ID
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @Column(name = "user_name")
     @NotNull(message = "用户名不能为空")
     @Length(min = 5, max = 20, message = "用户名长度必须介于 6 和 20 之间")
     private String userName;
 
+    @NotNull(message = "密码不能为空")
+    @JsonIgnore
     private String password;
 
     private String phone;
@@ -33,33 +30,6 @@ public class Users extends AbstractBaseDomain {
     @Pattern(regexp = RegexpUtils.EMAIL, message = "邮箱格式不正确")
     private String email;
 
-    private String creator;
-
-    @Column(name = "gmt_created")
-    private Date gmtCreated;
-
-    private String modifier;
-
-    @Column(name = "gmt_modified")
-    private Date gmtModified;
-
-    /**
-     * 获取主键ID
-     *
-     * @return id - 主键ID
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * 设置主键ID
-     *
-     * @param id 主键ID
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     /**
      * @return user_name
@@ -115,61 +85,5 @@ public class Users extends AbstractBaseDomain {
      */
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    /**
-     * @return creator
-     */
-    public String getCreator() {
-        return creator;
-    }
-
-    /**
-     * @param creator
-     */
-    public void setCreator(String creator) {
-        this.creator = creator;
-    }
-
-    /**
-     * @return gmt_created
-     */
-    public Date getGmtCreated() {
-        return gmtCreated;
-    }
-
-    /**
-     * @param gmtCreated
-     */
-    public void setGmtCreated(Date gmtCreated) {
-        this.gmtCreated = gmtCreated;
-    }
-
-    /**
-     * @return modifier
-     */
-    public String getModifier() {
-        return modifier;
-    }
-
-    /**
-     * @param modifier
-     */
-    public void setModifier(String modifier) {
-        this.modifier = modifier;
-    }
-
-    /**
-     * @return gmt_modified
-     */
-    public Date getGmtModified() {
-        return gmtModified;
-    }
-
-    /**
-     * @param gmtModified
-     */
-    public void setGmtModified(Date gmtModified) {
-        this.gmtModified = gmtModified;
     }
 }
