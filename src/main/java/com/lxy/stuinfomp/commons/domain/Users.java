9839364213f51b32299290f2a55894733d02ce07
robin_time <robin_time@163.com>
2019-08-12 00:33:25 +0800

    登录优化

--- src/main/java/com/lxy/stuinfomp/commons/domain/Users.java
+++ src/main/java/com/lxy/stuinfomp/commons/domain/Users.java
@@ -18,7 +18,7 @@ public class Users extends AbstractBaseDomain {
     @Column(name = "user_name")
     @NotNull(message = "用户名不能为空")
     @Length(min = 5, max = 20, message = "用户名长度必须介于 5 和 20 之间")
-    private String userName;
+    private String username;
 
     @NotNull(message = "密码不能为空")
     @JsonIgnore
@@ -26,23 +26,16 @@ public class Users extends AbstractBaseDomain {
 
     private String phone;
 
-//    @NotNull(message = "邮箱不能空")
+    @NotNull(message = "邮箱不能空")
     @Pattern(regexp = RegexpUtils.EMAIL, message = "邮箱格式不正确")
     private String email;
 
-
-    /**
-     * @return user_name
-     */
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    /**
-     * @param userName
-     */
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     /**
